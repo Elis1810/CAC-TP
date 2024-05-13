d1dae8adb0e2817ebe8febea093224c398df5990
eliana Maldonado <eliana.maldonado@outlook.es>
2024-05-13 15:43:39 -0300

    consumo de api para filtros

--- js/java.js
+++ js/java.js
@@ -99,7 +99,175 @@ function generarTarjetas(datosProductos) {
   return cad;
 }
 
+const { createApp } = Vue;
 
+createApp({
+  data() {
+    return {
+      url: "./js/api.json",
+      datos: [],
+      error: false,
+      filtroTipo: 'animales',
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.datos = data; // Guarda los datos en la variable datos
+        })
+        .catch(error => {
+          console.log("Error:" + error);
+          this.error = true;
+        });
+    },
+  },
+  computed: {
+    filteredDatos() {
+      return this.datos.filter(elemento => elemento.tipo === this.filtroTipo);
+    },
+  },
+  created() {
+    // Se ejecuta cada vez que se crea el objeto VUE
+    this.fetchData(this.url);         
+  },
+}).mount("#app");
 
+createApp({
+  data() {
+    return {
+      url: "./js/api.json",
+      datos: [],
+      error: false,
+      filtroTipo: 'anime',
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.datos = data; // Guarda los datos en la variable datos
+        })
+        .catch(error => {
+          console.log("Error:" + error);
+          this.error = true;
+        });
+    },
+  },
+  computed: {
+    filteredDatos() {
+      return this.datos.filter(elemento => elemento.tipo === this.filtroTipo);
+    },
+  },
+  created() {
+    // Se ejecuta cada vez que se crea el objeto VUE
+    this.fetchData(this.url);         
+  },
+}).mount("#appAnime");
 
+createApp({
+  data() {
+    return {
+      url: "./js/api.json",
+      datos: [],
+      error: false,
+      filtroTipo: 'personajes',
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.datos = data; // Guarda los datos en la variable datos
+        })
+        .catch(error => {
+          console.log("Error:" + error);
+          this.error = true;
+        });
+    },
+  },
+  computed: {
+    filteredDatos() {
+      return this.datos.filter(elemento => elemento.tipo === this.filtroTipo);
+    },
+  },
+  created() {
+    // Se ejecuta cada vez que se crea el objeto VUE
+    this.fetchData(this.url);         
+  },
+}).mount("#appPersonajes");
+
+createApp({
+  data() {
+    return {
+      url: "./js/api.json",
+      datos: [],
+      error: false,
+      filtroTipo: 'ramos',
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.datos = data; // Guarda los datos en la variable datos
+        })
+        .catch(error => {
+          console.log("Error:" + error);
+          this.error = true;
+        });
+    },
+  },
+  computed: {
+    filteredDatos() {
+      return this.datos.filter(elemento => elemento.tipo === this.filtroTipo);
+    },
+  },
+  created() {
+    // Se ejecuta cada vez que se crea el objeto VUE
+    this.fetchData(this.url);         
+  },
+}).mount("#appRamos");
+
+createApp({
+  data() {
+    return {
+      url: "./js/api.json",
+      datos: [],
+      error: false,
+      filtroTipo: 'varios',
+    };
+  },
+  methods: {
+    fetchData(url) {
+      fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          this.datos = data; // Guarda los datos en la variable datos
+        })
+        .catch(error => {
+          console.log("Error:" + error);
+          this.error = true;
+        });
+    },
+  },
+  computed: {
+    filteredDatos() {
+      return this.datos.filter(elemento => elemento.tipo === this.filtroTipo);
+    },
+  },
+  created() {
+    // Se ejecuta cada vez que se crea el objeto VUE
+    this.fetchData(this.url);         
+  },
+}).mount("#appVarios");
 
